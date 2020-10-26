7718f39f96ed8e384b3ab1e579c4366c0855908d
lilei <23399237@qq.com>
2020-10-26 10:58:24 +0800

    s

--- Test/src/com/it/test/Test.java
+++ Test/src/com/it/test/Test.java
@@ -8,21 +8,6 @@ package com.it.test;
 public class Test {
     public void main(String[]s){
         System.out.println("master02 ");
-        System.out.println("111aaaaa");
-        System.out.println("111aaaaaaaa");
-        System.out.println("111aaaaaaaaaaa");
-        System.out.println("111aaaaaaaaaaa2");
-        System.out.println("master01");
-        System.out.println("111123123123");
-        System.out.println("$$$$$$$$$$$$$");
-        System.out.println("$$$$$$$$$$$$$!!!!");
-        System.out.println("$$$$$$$$$$$$$!!!!@@");
-        System.out.println("$$$$$$$$$$$$$!!!!@@@");
-        System.out.println("$$$$$$$$$$$$$!!!!@@@");
-        System.out.println("$$$$$$$$$$$$$!!!!@@@");
-        System.out.println("$$$$$$$$$$$$$!!!!@@@");
-        System.out.println("master_02");
-        System.out.println("master02");
-        System.out.println("master02 ");
+     
     }
 }
