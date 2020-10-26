99903a109427e2df62864a9e96ef2108c5fe55fe
tony <23399237@qq.com>
2020-10-26 10:07:08 +0800

    t

---
+++ Test/src/com/it/test/Test.java
@@ -0,0 +1,12 @@
+package com.it.test;
+
+/**
+ * @description:
+ * @author: ll
+ * @create: 2020-10-26 10:06
+ */
+public class Test {
+    public void main(String[]s){
+        System.out.println("1");
+    }
+}
