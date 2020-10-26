f7bd06ecf9c30bebedaeb9a3d96d8e012d020b7c
lilei <23399237@qq.com>
2020-10-26 10:26:59 +0800

    a

--- Test/src/com/it/test/Test.java
+++ Test/src/com/it/test/Test.java
@@ -17,5 +17,6 @@ public class Test {
         System.out.println("$$$$$$$$$$$$$");
         System.out.println("$$$$$$$$$$$$$!!!!");
         System.out.println("$$$$$$$$$$$$$!!!!@@");
+        System.out.println("$$$$$$$$$$$$$!!!!@@@");
     }
 }
